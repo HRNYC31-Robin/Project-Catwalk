809d44bebfd4eb5af141b7f42a98675829c52380
Deo-Mac <druplall@gmail.com>
2020-08-04 23:51:21 -0400

    Create project scaffolding for FEC project

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> Deo </h1>
+        <h2> Test</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
