6bc81731fcfe438f0ab1c69a00ae3b394cd72463
Liam Murray <murrayl9797@gmail.com>
2020-08-11 09:10:10 -0400

    Showcase react utilizing redux store

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -31,7 +31,7 @@ class App extends React.Component {
   render() {
     return (
       <Container>
-          <Overview />
+          <Overview productName={!this.props.product ? 'Unknown' : this.props.product.name}/>
           <Products />
           <RatingsReviews />
       </Container>
--- client/src/components/Liam/Overview.jsx
+++ client/src/components/Liam/Overview.jsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css'; don't need?
 import Container from 'react-bootstrap/Container';
 
-const Overview = function () {
+const Overview = function ({productName}) {
   return (
     <>
       <Container className='overviewContainer'>
-        <h3> Overview goes here! </h3>
+        <h3> Overview goes here! Product is {productName} </h3>
       </Container>
     </>
   );
