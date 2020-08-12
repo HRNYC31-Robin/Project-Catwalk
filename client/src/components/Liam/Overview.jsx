53ea7af98a93f12fee19a5beb149ac738b5f08e5
Liam Murray <murrayl9797@gmail.com>
2020-08-11 20:29:06 -0400

    Initial grid set up of overview

--- client/src/components/Liam/Overview.jsx
+++ client/src/components/Liam/Overview.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 // import 'bootstrap/dist/css/bootstrap.min.css'; don't need?
 import Container from 'react-bootstrap/Container';
 
 const Overview = function ({currentProduct}) {
   return (
-      <Container className='overviewContainer'>
-        <h3> Overview goes here! {currentProduct.name} </h3>
-      </Container>
+    <Container className='overviewContainer'>
+      <div class="image-gallery">
+        Image Gallery
+      </div>
+
+      <div class="upper-right">
+        <div class="product-info">
+          Product-info
+        </div>
+        <div class="style-selector">
+          Style Selector
+        </div>
+        <div class="add-to-cart">
+          Add to cart!
+        </div>
+      </div>
+
+      <div class="product-blurb">
+        Product-Blurb
+      </div>
+
+      <div class="gmo-thing">
+        GMO
+      </div>
+    </Container>
   );
 };
 
