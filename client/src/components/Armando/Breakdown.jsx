5704b89bdbe35fc18c2e2192993c7aebc56b918e
Jorge Armando Garcia-Jacquier <j.armando.garcia.jacquier@gmail.com>
2020-08-12 18:21:40 -0400

    create rating bar component

--- client/src/components/Armando/Breakdown.jsx
+++ client/src/components/Armando/Breakdown.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import StarRating from '../common/StarRating.jsx';
+import RatingBar from './RatingBar.jsx';
 
 const Breakdown = (props) => {
+  console.log(props.ratings, 'check ratings exist');
+  // set a default
+  let ratings = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0};
+
+  // normalize the ratings
+  if (props.ratings) {
+    ratings = Object.assign({}, ratings, props.ratings);
+  }
+  console.log(ratings, 'normalized ratings');
 
   return (
     <div>
       {props.starAverage}
       <StarRating starAverage={props.starAverage} />
+
+      {
+        Object.keys(ratings).map(star => (
+          <RatingBar starNum={star} starRating={ratings[star]} totalRatings={props.totalRatings}/>
+        ))
+      }
+
     </div>
   );
 };
