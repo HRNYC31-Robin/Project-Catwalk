9ea5b8b44723727c1b6cf397fb2e418885c34a01
Jorge Armando Garcia-Jacquier <j.armando.garcia.jacquier@gmail.com>
2020-08-12 14:47:21 -0400

    Add date to ReviewTile Add stars to ReviewTile Add grid to ReviewTile

--- client/src/components/Armando/ReviewTile.jsx
+++ client/src/components/Armando/ReviewTile.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import StarRating from '../common/StarRating.jsx';
+
 
 const ReviewTile = (props) => {
+  const date = props.review.date.split('T')[0].split('-');
+  const formatedDate = new Date(Date.UTC(...date));
+  const options = {year: 'numeric', month: 'long', day: 'numeric' };
+  console.log(formatedDate.toLocaleDateString('en-US', options));
+  const dateToRender = formatedDate.toLocaleDateString('en-US', options);
+
+
   return (
-    <div key={props.review.review_id}>
+    <div id='review-tile' key={props.review.review_id}>
+      <StarRating id='stars' starAverage={props.review.rating}/>
+      <p id='date'>{dateToRender}</p>
       <h3>{props.review.summary}</h3>
       <p>{props.review.body}</p>
     </div>
-  )
+  );
 };
 
 export default ReviewTile;
