88cc3e2813be494b807273670f76b292c5b56d52
Jorge Armando Garcia-Jacquier <j.armando.garcia.jacquier@gmail.com>
2020-08-19 11:58:57 -0400

    Dropdown works -- saving progress

--- client/src/components/Armando/ReviewList.jsx
+++ client/src/components/Armando/ReviewList.jsx
@@ -6,19 +6,21 @@ import ReviewForm from './ReviewForm.jsx';
 const ReviewList = (props) => {
   const prodId = props.currentProduct.id;
   const [reviewModal, setReviewModal] = useState(false);
-  const [sortList, setSortList] = useState('helpful');
+  const [sortList, setSortList] = useState('relevant');
 
 
   // updated the reviews if product Id changes
   useEffect(() => {
-    axios.get(`http://18.224.37.110/reviews/?product_id=${prodId}&sort=${sortList}`)
+    // set a default for count for first request
+    const count = props.totalRatings || 5;
+
+    axios.get(`http://18.224.37.110/reviews/?product_id=${prodId}&sort=${sortList}&count=${count}`)
       .then(results => {
-        console.log('This is first get in ReviewList prodId', props.totalRatings);
         props.addMoreReviews(results.data.results);
         props.handleMoreReviewsClick(results.data.results.slice(0, 2));
       })
       .catch(err => console.log(err));
-  }, [prodId]);
+  }, [prodId, sortList]);
 
   const handleMoreClick = () => {
     if (props.reviews.length <= props.totalRatings) {
@@ -41,6 +43,11 @@ const ReviewList = (props) => {
 
   return (
     <div>
+      <select onChange={(e) => setSortList(e.target.value)}>
+        <option key='relevant' value='relevant'>Relevant</option>
+        <option key='helpful' value='helpful'>Helpful</option>
+        <option key='newest' value='newest'>Newest</option>
+      </select>
       {
         reviewModal ? <ReviewForm metaData={props.ratingsMeta} prodName={props.currentProduct.name} handleClose={() => setReviewModal(false)} /> : null
       }
