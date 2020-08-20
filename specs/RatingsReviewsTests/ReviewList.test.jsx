a78c33bbd3ac456867a2cd13189a7872bf8d11b8
Jorge Armando Garcia-Jacquier <j.armando.garcia.jacquier@gmail.com>
2020-08-20 19:51:35 -0400

    add test for no reviewTile

--- specs/RatingsReviewsTests/ReviewList.test.jsx
+++ specs/RatingsReviewsTests/ReviewList.test.jsx
@@ -11,6 +11,12 @@ console.log(reviewsTest);
 
 test('Should have reviewTiles', () => {
   const wrapper = shallow(<ReviewList ratingsMeta={{}} currentProduct={testData[0]} reviews={reviewsTest} visibleReviews={reviewsTest.slice(0, 2)}/>);
-  console.log(wrapper.debug());
-  expect(wrapper.find('ReviewTile').length).toEqual(2);
+  // console.log(wrapper.debug());
+  expect(wrapper.find('ReviewTile').length).not.toBe(0);
+});
+
+test('Should have no reviewTiles', () => {
+  const wrapper = shallow(<ReviewList ratingsMeta={{}} currentProduct={testData[0]} reviews={reviewsTest} visibleReviews={[]}/>);
+  // console.log(wrapper.debug());
+  expect(wrapper.find('ReviewTile').length).toBe(0);
 });
